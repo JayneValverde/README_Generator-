b3f2a2551e62600e422ec84d515a9cb6bae97c54
Jayne Valverde <jaynevalverde@gmail.com>
2023-06-17 21:26:40 -0500

    major edits to project

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
 
+// External packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Internal module 
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+const questions =   [{
+            type: 'input',
+            message: 'What is the Project Title?',
+            name: 'Title'
+        },
+        {
+            type: 'input',
+            message: 'What is the project about? Detailed description.',
+            name: 'Description',
+        },
+        {
+            type: 'input',
+            message: 'What does the user need to install to run this app? (ie..dependencies)?',
+            name: 'Installation',
+        },
+        {
+            type: 'input',
+            message: 'How is the app used? Give Instructions.',
+            name: 'Usage',
+        },
+        {
+            type: 'input',
+            message: 'If applicable, list contributions to this project.',
+            name: 'Contributing',
+        },
+        {
+            type: 'input',
+            message: 'What command are needed to test this app?',
+            name: 'Tests',
+        },
+        {
+            type: 'list',
+            message: "Choose a license for your project",
+            choices: ['Apache 2.0', 'Boost Software License 1.0', 'Eclipse Publice License 1.0', 'GNU GPLv3', 'GNU AGPLv3', 'the MIT License','The Unlicense' ],
+            name: 'License',
+        }
+    ]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if(err) {
+            return console.log(err)
+        } else {
+            console.log("Success! Your README.md file has been generated")
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(function(data) {
+            writeToFile("README.md", generateMarkdown(data));
+            console.log(data)
+
+        })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,21 +1,99 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = '';
+    if(license === 'Apache') {
+        badge = `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+    } else if (license === 'BootSoftWare') {
+        badge = `![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`
+    } else if (license ==='Eclipse') {
+        badge = `![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)`
+    } else if (license === 'GNUGPLv3') {
+        badge = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+    } else if (license === 'GNUAGPLv3') {
+        badge = `![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)`
+    } else if (license === 'MIT') {
+        badge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+    } else if (license === 'Unlicense') {
+        badge = `![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)`
+    } else {
+      badge = ''
+    }
+    return badge;
+  }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let LicenseLink = '';
+    if(license === 'Apache') {
+        LicenseLink = `https://opensource.org/licenses/Apache-2.0`
+    } else if (license === 'BootSoftWare') {
+        LicenseLink = `https://www.boost.org/LICENSE_1_0.txt`
+    } else if (license ==='Eclipse') {
+        LicenseLink = `https://opensource.org/licenses/EPL-1.0`
+    } else if (license === 'GNUGPLv3') {
+        LicenseLink = `https://www.gnu.org/licenses/gpl-3.0`
+    } else if (license === 'GNUAGPLv3') {
+        LicenseLink = `https://www.gnu.org/licenses/agpl-3.0`
+    } else if (license === 'MIT') {
+        LicenseLink = `https://opensource.org/licenses/MIT`
+    } else if (license === 'Unlicense') {
+        LicenseLink = `http://unlicense.org/`
+    } else {
+        LicenseLink = ''
+    }
+    return LicenseLink;
+  }
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = ''
+  if(license === 'None') {
+    licenseSection = ''
+  } else {
+    licenseSection = 
+    `License: ${license}`
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.Title}
 
-`;
+  ## ${renderLicenseSection(data.license)} ${renderLicenseBadge(data.license)}
+  ### ${renderLicenseLink(data.license)}
+
+  # Description
+  ${data.Description}
+
+  # Table of Contents
+  * [Installations](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  
+  # Installation
+  The following dependencies ust be installed to run the application
+
+  # Usage
+  In order to use this App, ${data.Usage}
+
+  # Contributors: ${data.Contributing}
+
+  # Tests
+  The Following is needed to run tests on application: ${data.Tests}
+
+  # License
+  This project is license under ${data.License}
+  `
 }
 
 module.exports = generateMarkdown;
+
